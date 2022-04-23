f4c10d2cbde0c2bb714fd5c8ae1e345caf84d52e
Ruslan <ruslan.shtybaev@gmail.com>
2022-04-23 13:23:21 +0300

    Initial Commit

---
+++ RickAndMorty2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty2022
+//
+//  Created by Руслан Штыбаев on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
